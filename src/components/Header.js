68db014838cd0616d3275669ec9670e89b74de08
souz-dev <souz.developer@gmail.com>
2021-04-15 12:34:12 -0300

    Feature: Delicious Fruit version 1.0

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,8 @@ import '../styles/components/Header.modules.css'
 export function Header() {
   return (
     <header className="header">
-      <h1>Delicious fruits </h1>
+      <h1>Delicious fruits 
+      </h1>
       <div></div>
     </header>
   )
